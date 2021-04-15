fd20626069e9d541790da908994afdea8960d9ce
NatalliaSaverchenko <saverchenko.nn@gmail.com>
2021-04-15 22:42:36 +0300

    get angle between vectors done

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -119,7 +119,10 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  throw new Error('Not implemented')
+  let a_mod = Math.sqrt(x1 ** 2 + y1 ** 2)
+  let b_mod = Math.sqrt(x2 ** 2 + y2 ** 2)
+  let a_b_multiply = x1 * x2 + y1 * y2
+  return Math.acos(a_b_multiply / (a_mod * b_mod))
 }
 
 /**
