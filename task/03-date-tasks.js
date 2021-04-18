18f1dac63629ecfcc23e01d6547b8b582f0e959e
NatalliaSaverchenko <saverchenko.nn@gmail.com>
2021-04-19 00:13:46 +0300

    is leap year

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -56,7 +56,17 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-  throw new Error('Not implemented')
+  let arg = new Date(date)
+  let year = arg.getFullYear()
+
+  if (year % 4 == 0 && year % 100 != 0) {
+    return true
+  }
+
+  if (year % 400 == 0) {
+    return true
+  }
+  return false
 }
 
 /**
